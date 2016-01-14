f48fda420a7330607024f0f2287ec4cf6c464c19
Hutson Betts <hutson@hyper-expanse.net>
2016-01-13 21:38:35 -0600

    test(index): Complete 100% test coverage.

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,23 +1,58 @@
 'use strict';
 
-var expect = require('chai').expect;
+var chai = require('chai');
 var proxyquire = require('proxyquire');
 var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
+
+chai.use(sinonChai);
+var expect = chai.expect;
+
+var gulplog = {
+  warn: sinon.spy(),
+  info: sinon.spy(),
+  '@noCallThru': true,
+};
 
 var GulplogLogger = proxyquire('./index.js', {
-  gulplog: {
-    warn: sinon.spy(),
-    info: sinon.spy(),
-    '@noCallThru': true,
-  },
+  gulplog: gulplog,
 });
 
 describe('GulplogLogger', function() {
   var gulplogLogger;
 
+  beforeEach(function() {
+    gulplog.warn.reset();
+    gulplog.info.reset();
+  });
+
   it('has a default log level of info', function() {
     gulplogLogger = new GulplogLogger();
 
     expect(gulplogLogger.level).to.equal('info');
+  });
+
+  it('logs to `gulplog`', function() {
+    var callback = sinon.spy();
+    gulplogLogger = new GulplogLogger();
+
+    gulplogLogger.log('warn', 'test message', {}, callback);
+
+    expect(gulplog.warn).to.have.been.calledOnce;
+    expect(gulplog.info).to.not.have.been.called;
+    expect(callback).to.have.been.calledOnce
+      .and.to.be.calledWithExactly(null, true);
+  });
+
+  it('defaults to info if invalid log level provided', function() {
+    var callback = sinon.spy();
+    gulplogLogger = new GulplogLogger();
+
+    gulplogLogger.log('invalid', 'test message', {}, callback);
+
+    expect(gulplog.warn).to.not.have.been.called;
+    expect(gulplog.info).to.have.been.calledOnce;
+    expect(callback).to.have.been.calledOnce
+      .and.to.be.calledWithExactly(null, true);
   });
 });
